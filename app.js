31226db3fbd32e6732638bb107d24cd3bede248f
Matt <wrestler.411@comcast.net>
2017-10-04 14:09:37 -0700

    got the list working and no duplicates

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 'use strict';
 
-var counter = 0;
 Product.allItems = [];
 Product.lastDisplayed = [];
 Product.totalClicks = 0;
 Product.section = document.getElementById('product-section');
-Product.results = document.getElementById('results');
+Product.resultsList = document.getElementById('results');
 
 function Product(name, filepath){
-// function Product(name, filepath, votes, views){
   this.name = name;
   this.filepath = filepath;
-  // this.votes = userClicks;
-  // this.views = userViews;
+  this.timesDisplayed = 0;
+  this.votes = 0;
   Product.allItems.push(this);
 };
 
@@ -37,62 +35,70 @@ new Product('usb', 'img/usb.gif');
 new Product('water-can', 'img/water-can.jpg');
 new Product('wine-glass', 'img/wine-glass.jpg');
 
-// new Product('bag', 'img/bag.jpg', userClicks, userViews);
-// new Product('banana', 'img/banana.jpg', userClicks, userViews);
-// new Product('bathroom', 'img/bathroom.jpg', userClicks, userViews);
-// new Product('boots', 'img/boots.jpg', userClicks, userViews);
-// new Product('breakfast', 'img/breakfast.jpg', userClicks, userViews);
-// new Product('bubblegum', 'img/bubblegum.jpg', userClicks, userViews);
-// new Product('chair', 'img/chair.jpg', userClicks, userViews);
-// new Product('cthulhu', 'img/cthulhu.jpg', userClicks, userViews);
-// new Product('dog-duck', 'img/dog-duck.jpg', userClicks, userViews);
-// new Product('dragon', 'img/dragon.jpg', userClicks, userViews);
-// new Product('pen', 'img/pen.jpg', userClicks, userViews);
-// new Product('pet-sweep', 'img/pet-sweep.jpg', userClicks, userViews);
-// new Product('scissors', 'img/scissors.jpg', userClicks, userViews);
-// new Product('shark', 'img/shark.jpg', userClicks, userViews);
-// new Product('sweep', 'img/sweep.png', userClicks, userViews);
-// new Product('tauntaun', 'img/tauntaun.jpg', userClicks, userViews);
-// new Product('unicorn', 'img/unicorn.jpg', userClicks, userViews);
-// new Product('usb', 'img/usb.gif', userClicks, userViews);
-// new Product('water-can', 'img/water-can.jpg', userClicks, userViews);
-// new Product('wine-glass', 'img/wine-glass.jpg', userClicks, userViews);
-var randomArray = [];
+
 var imgEl1 = document.getElementById('item1');
 var imgEl2 = document.getElementById('item2');
 var imgEl3 = document.getElementById('item3');
 function randomProduct(){
-  if (counter < 25){
-    randomArray.shift();
-    randomArray.shift();
-    randomArray.shift();
-    var randomIndex1 = Math.floor(Math.random() * Product.allItems.length);
-    imgEl1.src = Product.allItems[randomIndex1].filepath;
-    imgEl1.addEventListener('click', randomProduct);
-    var randomIndex2 = Math.floor(Math.random() * Product.allItems.length);
-    imgEl2.src = Product.allItems[randomIndex2].filepath;
-    imgEl2.addEventListener('click', randomProduct);
-    var randomIndex3 = Math.floor(Math.random() * Product.allItems.length);
-    imgEl3.src = Product.allItems[randomIndex3].filepath;
-    imgEl3.addEventListener('click', randomProduct);
-    counter++;
-    randomArray.push(randomIndex1, randomIndex2, randomIndex3);
-    console.log(randomArray);
-  } else{
-    console.log('Yeet');
-  };
+  var randomIndex1 = Math.floor(Math.random() * Product.allItems.length);
+  var randomIndex2 = Math.floor(Math.random() * Product.allItems.length);
+  var randomIndex3 = Math.floor(Math.random() * Product.allItems.length);
+  while(Product.lastDisplayed.includes(randomIndex1) || Product.lastDisplayed.includes(randomIndex2) || Product.lastDisplayed.includes(randomIndex3) || randomIndex1 === randomIndex2 || randomIndex1 === randomIndex3 || randomIndex2 === randomIndex3){
+    randomIndex1 = Math.floor(Math.random() * Product.allItems.length);
+    randomIndex2 = Math.floor(Math.random() * Product.allItems.length);
+    randomIndex3 = Math.floor(Math.random() * Product.allItems.length);
+  }
+
+  imgEl1.src = Product.allItems[randomIndex1].filepath;
+  imgEl1.id = Product.allItems[randomIndex1].name;
+  imgEl2.src = Product.allItems[randomIndex2].filepath;
+  imgEl2.id = Product.allItems[randomIndex2].name;
+  imgEl3.src = Product.allItems[randomIndex3].filepath;
+  imgEl3.id = Product.allItems[randomIndex3].name;
+  Product.allItems[randomIndex1].timesDisplayed += 1;
+  Product.allItems[randomIndex2].timesDisplayed += 1;
+  Product.allItems[randomIndex3].timesDisplayed += 1;
+  Product.lastDisplayed[0] = randomIndex1;
+  Product.lastDisplayed[1] = randomIndex2;
+  Product.lastDisplayed[2] = randomIndex3;
 };
 
-while(Product.lastDisplayed.includes(randomIndex1) || Product.lastDisplayed.includes(randomIndex2) || Product.lastDisplayed.includes(randomIndex3) || randomIndex1 === randomIndex2 || randomIndex1 === randomIndex3 || randomIndex2 === randomIndex3){
-  randomIndex1 = Math.floor(Math.random() * Product.allItems.length);
-  randomIndex2 = Math.floor(Math.random() * Product.allItems.length);
-  randomIndex3 = Math.floor(Math.random() * Product.allItems.length);
+
+
+
+function handleClick(e) {
+  if(e.target.id === 'product-section') {
+    return alert('Please click on a product to choose it!');
+  }
+
+  Product.totalClicks += 1;
+
+  console.log(e.target, 'e.target');
+  for(var i = 0; i < Product.allItems.length; i++) {
+    if(e.target.id === Product.allItems[i].name) {
+      Product.allItems[i].votes += 1;
+      console.log(Product.allItems[i].votes, 'votes');
+    }
+  }
+
+  if(Product.totalClicks > 24) {
+    Product.section.removeEventListener('click', handleClick);
+    showResults();
+  }
+
+  randomProduct();
 }
 
+function showResults() {
+  for(var i = 0; i < Product.allItems.length; i++) {
+    var liEl = document.createElement('li');
+    liEl.textContent = Product.allItems[i].name + ' has ' + Product.allItems[i].votes + ' votes in ' + Product.allItems[i].timesDisplayed + ' times shown.';
+    Product.resultsList.appendChild(liEl);
+  }
+}
 
-Product.lastDisplayed[0] = randomIndex1;
-Product.lastDisplayed[1] = randomIndex2;
-Product.lastDisplayed[2] = randomIndex3;
+Product.section.addEventListener('click', handleClick);
+
 
 
 randomProduct();
